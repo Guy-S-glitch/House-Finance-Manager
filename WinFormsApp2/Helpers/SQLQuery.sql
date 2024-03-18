19d5c1c190c84b64a5d2fe810826d30faeddbc01
Guy-S-glitch <guysolomon365@gmail.com>
2024-03-19 00:02:52 +0200

    added the query needed to create the table

---
+++ WinFormsApp2/Helpers/SQLQuery.sql
@@ -0,0 +1,24 @@
+﻿create database HouseDB;
+go
+use [HouseDB];
+
+CREATE TABLE [dbo].[Houses](
+[HouseNumber] nvarchar NOT NULL,
+[memberName] nvarchar NOT NULL,
+[Birth] [datetime] NOT NULL,
+[Gender] nvarchar NOT NULL,
+[Picture] varbinary NOT NULL,
+[Job] nvarchar NOT NULL,
+[Experience] [int] NOT NULL,
+[Salary] [int] NOT NULL,
+[City] nvarchar NOT NULL,
+[Phone] nvarchar NOT NULL,
+[Email] nvarchar NOT NULL,
+[Transport] [int] NOT NULL,
+[Clothes] [int] NOT NULL,
+[Sport] [int] NOT NULL,
+[Market] [int] NOT NULL,
+[Utilities] [int] NOT NULL,
+[Rent] [int] NOT NULL,
+[Restaurant] [int] NOT NULL
+);
